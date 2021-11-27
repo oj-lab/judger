26d91653cd348ad29ba550136b6b7e25930f6d66
slhmy <1484836413@qq.com>
2021-11-27 18:49:02 +0800

    Add seccomp example

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 [![Tests](https://img.shields.io/github/workflow/status/slhmy/judger-rs/Build)](https://github.com/slhmy/judger-rs/actions/workflows/rust_build.yml)
 
 Judger is supposed to be a simple **sandbox service** which works for online-judge systems.
+
+## Run by admin
+
+`judge-core` build a sandbox environment by `seccomp`, so it's neccessary to run tests or examples by admin.
+You need to install **Rust** in root user.
+``` sh
+su
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
---
+++ judge-core/examples/seccomp.rs
@@ -0,0 +1,16 @@
+use seccomp::*;
+
+fn main() {
+    let mut ctx = Context::default(Action::Allow).unwrap();
+    let rule = Rule::new(libc::SYS_setuid as usize /* setuid on x86_64 */,
+        Compare::arg(0)
+                .with(1000)
+                .using(Op::Eq)
+                .build().unwrap(),
+        Action::Errno(libc::EPERM) /* return EPERM */
+    );
+    ctx.add_rule(rule).unwrap();
+    ctx.load().unwrap();
+    let ret = unsafe { libc::setuid(1000) };
+    println!("ret = {}, uid = {}", ret, unsafe { libc::getuid() });
+}
