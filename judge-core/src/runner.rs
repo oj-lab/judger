3f2eb9578b727114f0a52bc1a000a1e5c3c769f8
slhmy <1484836413@qq.com>
2022-01-22 15:50:22 +0000

    fmt

--- judge-core/src/runner.rs
+++ judge-core/src/runner.rs
@@ -1,25 +1,22 @@
 use nix::{
+    errno::Errno,
     sys::resource::{
         setrlimit,
         Resource::{RLIMIT_AS, RLIMIT_CPU, RLIMIT_FSIZE, RLIMIT_NPROC, RLIMIT_STACK},
     },
-    unistd::execve,
-    errno::Errno,
     unistd::dup2,
-};
-use std::os::unix::io::{
-    RawFd,
-    AsRawFd
+    unistd::execve,
 };
 use std::ffi::CString;
 use std::fs::File;
 use std::io;
+use std::os::unix::io::{AsRawFd, RawFd};
 
 pub fn run_process() {
     // TODO: Handle error
     set_limit().unwrap();
 
-    let input_file  = File::open("../tmp/in").unwrap();
+    let input_file = File::open("../tmp/in").unwrap();
     let output_file = File::options().write(true).open("../tmp/out").unwrap();
 
     let input_raw_fd: RawFd = input_file.as_raw_fd();
