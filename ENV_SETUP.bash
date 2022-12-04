857e6e2900932ce36638c2bc2f9d7a25de37c11e
slhmy <31381093+slhmy@users.noreply.github.com>
2022-12-04 12:47:56 +0000

    Add  ENV_SETUP bash

---
+++ ENV_SETUP.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+UBUNTU_FLAG="ubuntu"
+
+CUR_SYSTEM=$(uname -a | tr '[:upper:]' '[:lower:]')
+
+if [[ $CUR_SYSTEM =~ $UBUNTU_FLAG ]];then
+    echo Current system is Ubuntu
+    sudo apt update
+    sudo apt install libseccomp-dev
+fi
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
