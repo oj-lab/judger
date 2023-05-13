4e83b4f6713f67ce4bc470da5ad760fa91d4a618
slhmy <1484836413@qq.com>
2023-05-13 15:38:59 +0800

    Do not install protobuf-compiler

--- ENV_SETUP.bash
+++ ENV_SETUP.bash
@@ -7,7 +7,7 @@ CUR_SYSTEM=$(uname -a | tr '[:upper:]' '[:lower:]')
 if [[ $CUR_SYSTEM =~ $UBUNTU_FLAG ]];then
     echo Current system is Ubuntu
     sudo apt update
-    sudo apt install -y libseccomp-dev gcc protobuf-compiler
+    sudo apt install -y libseccomp-dev gcc
 fi
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
