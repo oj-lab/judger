93950668289ebd6fde616192a41abcc70a9beade
slhmy <1484836413@qq.com>
2023-06-17 09:34:31 +0800

    Perf utils

--- judge-core/src/utils.rs
+++ judge-core/src/utils.rs
@@ -1,33 +1,5 @@
 use anyhow::anyhow;
-use libc::rusage;
 use std::{fs, io, path::PathBuf};
-
-pub fn get_default_rusage() -> rusage {
-    rusage {
-        ru_utime: libc::timeval {
-            tv_sec: 0,
-            tv_usec: 0,
-        },
-        ru_stime: libc::timeval {
-            tv_sec: 0,
-            tv_usec: 0,
-        },
-        ru_maxrss: 0,
-        ru_ixrss: 0,
-        ru_idrss: 0,
-        ru_isrss: 0,
-        ru_minflt: 0,
-        ru_majflt: 0,
-        ru_nswap: 0,
-        ru_inblock: 0,
-        ru_oublock: 0,
-        ru_msgsnd: 0,
-        ru_msgrcv: 0,
-        ru_nsignals: 0,
-        ru_nvcsw: 0,
-        ru_nivcsw: 0,
-    }
-}
 
 pub fn copy_recursively(src: &PathBuf, dest: &PathBuf) -> io::Result<()> {
     log::debug!("copying {:?} to {:?}", src, dest);
