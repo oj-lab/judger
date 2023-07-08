76fca4b38942b8af99b1faaaa70246ab1ead0bda
slhmy <31381093+slhmy@users.noreply.github.com>
2023-07-08 11:25:39 +0800

    Add some example doc (#109)

--- judge-core/src/lib.rs
+++ judge-core/src/lib.rs
@@ -1,6 +1,9 @@
 pub mod builder;
+/// Helper for compiling source into an executable
 pub mod compiler;
 pub mod error;
-pub mod judge;
+/// Prepared functions for running one single judge
+pub mod judge; 
+/// Runtime essentials for running a program under control
 pub mod run;
 pub mod utils;
