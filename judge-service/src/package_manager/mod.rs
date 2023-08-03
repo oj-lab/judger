1f010ff8ad8353f1a2b3ef509298fc42d41c70d6
slhmy <31381093+slhmy@users.noreply.github.com>
2023-08-03 03:29:02 +0000

    Fix some logic in PackageManager new

--- judge-service/src/package_manager/mod.rs
+++ judge-service/src/package_manager/mod.rs
@@ -13,22 +13,30 @@ pub struct PackageManager {
 
 impl PackageManager {
     pub fn new(folder_path: PathBuf) -> Result<Self, JudgeCoreError> {
-        if !folder_path.exists() || folder_path.is_file() {
+        if folder_path.exists() && folder_path.is_file() {
             return Err(JudgeCoreError::AnyhowError(anyhow::anyhow!(
-                "Package folder not found (or the path is a file): {}",
+                "Package folder '{}' appears to be a file.",
                 folder_path.display()
             )));
         }
 
-        let discription_file_path = folder_path.join(discription::PACKAGES_DISCRIPTION_FILE_NAME);
-        if !discription_file_path.exists() || discription_file_path.is_file() {
-            return Err(JudgeCoreError::AnyhowError(anyhow::anyhow!(
-                "Package discription file not found (or the path is not a file): {}",
-                discription_file_path.display()
-            )));
+        if !folder_path.exists() {
+            std::fs::create_dir_all(&folder_path)?;
         }
 
-        let package_discription_map = StoragedPackageDiscriptionMap::init(folder_path.clone())?;
+        let discription_file_path = folder_path.join(discription::PACKAGES_DISCRIPTION_FILE_NAME);
+        if discription_file_path.exists() && discription_file_path.is_dir() {
+            return Err(JudgeCoreError::AnyhowError(anyhow::anyhow!(
+                "Discription file '{}' appears to be a folder.",
+                folder_path.display()
+            )));
+        }
+        let package_discription_map;
+        if !discription_file_path.exists() {
+            package_discription_map = StoragedPackageDiscriptionMap::init(folder_path.clone())?;
+        } else {
+            package_discription_map = StoragedPackageDiscriptionMap::load(folder_path.clone())?;
+        }
 
         Ok(Self {
             folder_path,
