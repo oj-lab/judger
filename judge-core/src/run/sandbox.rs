fc754966c03ba00d691fe5e2c174ff2e4403a20e
slhmy <1484836413@qq.com>
2023-10-22 20:37:12 +0800

    Add allowed SCMP `clock_nanosleep`

--- judge-core/src/run/sandbox.rs
+++ judge-core/src/run/sandbox.rs
@@ -191,7 +191,7 @@ fn get_default_rusage() -> rusage {
     }
 }
 
-const DEFAULT_SCMP_WHITELIST: [&str; 27] = [
+const DEFAULT_SCMP_WHITELIST: [&str; 28] = [
     "read",
     "fstat",
     "mmap",
@@ -219,4 +219,5 @@ const DEFAULT_SCMP_WHITELIST: [&str; 27] = [
     "prlimit64",
     "futex",
     "openat",
+    "clock_nanosleep"
 ];
