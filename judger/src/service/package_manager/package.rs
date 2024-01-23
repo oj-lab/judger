077c5e469d435d784ff397112be589f91d674e20
KanadeSiina <lluozheyan@gmail.com>
2024-01-23 08:52:09 +0000

    Perf minio config file name

--- judger/src/service/package_manager/package.rs
+++ judger/src/service/package_manager/package.rs
@@ -22,9 +22,10 @@ fn check_rclone(config_path: &Path) -> Result<(), JudgeServiceError> {
 }
 
 pub const PACKAGE_SAVE_DIRNAME: &str = "rclone-problem-package";
+pub const RCLONE_CONFIG_FILE: &str = "rclone-minio.conf";
 
 pub fn sync_package(data_dir: &Path, bucket_name: &str) -> Result<(), JudgeServiceError> {
-    let config_path = data_dir.join("rclone-minio.conf");
+    let config_path = data_dir.join(RCLONE_CONFIG_FILE);
     let package_path = data_dir.join(PACKAGE_SAVE_DIRNAME);
     let check_res = check_rclone(&config_path);
     if check_res.as_ref().is_err() {
